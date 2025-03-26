6522c14cf7b024e465808d7e6fd62ceeaae2a5fd
frix4224 <omarfrix@goosetrp.com>
2025-03-27 00:35:05 +0100

    Copy lib/storage.ts from driver-app-v1

---
+++ lib/storage.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class Storage {
+  async setItem(key: string, value: string): Promise<void> {
+    await AsyncStorage.setItem(key, value);
+  }
+
+  async getItem(key: string): Promise<string | null> {
+    return await AsyncStorage.getItem(key);
+  }
+
+  async removeItem(key: string): Promise<void> {
+    await AsyncStorage.removeItem(key);
+  }
+}
+
+export const storage = new Storage();
